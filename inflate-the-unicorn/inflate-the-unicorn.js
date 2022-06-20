d2c43ae2004940528e59cb05d0d61c707f900d7f
michael henrys <michael42h@gmail.com>
2022-06-21 11:14:31 +1200

    Added solution to inflate-the-unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -2,4 +2,21 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
+let images = document.getElementsByClassName("inflate-an-image")
+let unicorns = []
+
+function clickEvent(e) {
+  let unicorn = unicorns.find(unicorn => unicorn.image == e.target)
+  if (unicorn.level < 3) unicorn.level++
+  unicorn.image.src = './images/unicorn-' + unicorn.level + '.png'
+}
+
+for (let i = 0; i < images.length; i++) {
+  images[i].onclick = clickEvent
+  unicorns.push({
+    image: images[i],
+    level: 0
+  })
+}
+
 console.log("Inflate The Unicorn!")
